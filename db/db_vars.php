8a01520147dd84988cbfcae4eb2b0bf4988a8275
Jenner12 <jjsanchez@galileo.edu>
2018-03-11 20:38:09 -0600

    prueba

--- db/db_vars.php
+++ db/db_vars.php
@@ -1,5 +1,5 @@
 <?php
-	define("URL_REDIRECT", "192.168.0.12");
+	define("URL_REDIRECT", "192.168.0.2");
 	define("DB_HOST", "localhost");
 	define("DB_NAME", "usercreator");
 	define("DB_USER", "root");
