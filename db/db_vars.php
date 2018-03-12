fd68896b4a56ad8bb3566ac9915a72dc4992c33e
Jenner12 <jjsanchez@galileo.edu>
2018-03-12 12:11:24 -0600

    IP redirect chex

--- db/db_vars.php
+++ db/db_vars.php
@@ -1,5 +1,5 @@
 <?php
-	define("URL_REDIRECT", "192.168.0.12");
+	define("URL_REDIRECT", $_SERVER["SERVER_ADDR"]);
 	define("DB_HOST", "localhost");
 	define("DB_NAME", "usercreator");
 	define("DB_USER", "root");
