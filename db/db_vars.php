92f5dc318e6a7cf22024b119ca5effe95c0b79c7
jjse12 <jjse127@gmail.com>
2018-04-23 20:03:18 -0600

    blah

--- db/db_vars.php
+++ db/db_vars.php
@@ -2,11 +2,11 @@
 	define("DB_HOST", "localhost");
 	define("DB_NAME", "chex");
 
-    define("URL_REDIRECT", $_SERVER["SERVER_NAME"]);
-    define("DB_USER", "homestead");git
-    define("DB_PASS", "secret");
+    //define("URL_REDIRECT", $_SERVER["SERVER_NAME"]);
+    //define("DB_USER", "homestead");
+    //define("DB_PASS", "secret");
 
-	//define("URL_REDIRECT", $_SERVER["SERVER_NAME"]."/chex");
-	//define("DB_USER", "root");
-	//define("DB_PASS", "chex2018");
+	define("URL_REDIRECT", $_SERVER["SERVER_NAME"]."/chex");
+	define("DB_USER", "root");
+	define("DB_PASS", "chex2018");
 ?>
