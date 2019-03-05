cd4c6a6bc09368d46acb4fc6d2cada56e63b60df
jjse12 <jjse127@gmail.com>
2019-03-04 23:05:23 -0600

    Ajustar zindex de opciones de tabla

--- views/inventario.php
+++ views/inventario.php
@@ -715,7 +715,7 @@
     </table>
 </div>
 
-<div class="container" align="center" style="background-color: white; position: fixed; left: 0; right: 0; bottom: 0; padding-bottom: 6px; z-index: 10026; visibility: hidden;" id="divBotones">
+<div class="container" align="center" style="background-color: white; position: fixed; left: 0; right: 0; bottom: 0; padding-bottom: 6px; z-index: 100; visibility: hidden;" id="divBotones">
     <div class='col-lg-12 col-md-12 col-sm-12 col-xs-12' style="padding-top: 6px;">
         <div class='col-lg-2 col-md-2 col-sm-2'></div>
         <div class='col-lg-8 col-md-8 col-sm-8 col-xs-12'>
