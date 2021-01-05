b1734e7ffb690cd88c26819b52c701bcb0d6ed0e
jjse12 <jjse127@gmail.com>
2021-01-05 09:31:40 -0600

    Modificar el tipo de dato de columna "fecha_poliza" de datetime a varchar(10)

--- migrations/tarifacion_paquete_express_columna_fecha_poliza.sql
+++ migrations/tarifacion_paquete_express_columna_fecha_poliza.sql
@@ -1,7 +1,7 @@
 alter table tarifacion_paquete_express
-    add fecha_poliza datetime null after poliza;
+    add fecha_poliza varchar(10) null after poliza;
 
-update tarifacion_paquete_express set fecha_poliza = fecha_creacion where 1;
+update tarifacion_paquete_express set fecha_poliza = DATE_FORMAT(fecha_creacion, '%d/%m/%Y') where 1;
 
-alter table tarifacion_paquete_express modify fecha_poliza datetime not null;
+alter table tarifacion_paquete_express modify fecha_poliza varchar(10) not null;
 
