bc8bfd9aa912581aa0b93f4ac0715f787b10f17c
jjse12 <jjse127@gmail.com>
2021-01-05 07:43:21 -0600

    Mejorar archivo de migración

--- migrations/tarifacion_paquete_express_columna_fecha_poliza.sql
+++ migrations/tarifacion_paquete_express_columna_fecha_poliza.sql
@@ -1,3 +1,7 @@
 alter table tarifacion_paquete_express
-    add fecha_poliza datetime not null after poliza;
+    add fecha_poliza datetime null after poliza;
+
+update tarifacion_paquete_express set fecha_poliza = fecha_creacion where 1;
+
+alter table tarifacion_paquete_express modify fecha_poliza datetime not null;
 
