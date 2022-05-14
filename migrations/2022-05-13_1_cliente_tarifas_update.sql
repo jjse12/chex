82fbb41289a20c2765f5d7efec9f08ed03b3be01
Jenner <sanchezjenner@hotmail.com>
2022-05-14 14:35:32 -0600

    Add migration for new tarifas management

---
+++ migrations/2022-05-13_1_cliente_tarifas_update.sql
@@ -0,0 +1,9 @@
+update cliente set tarifa_express = 25 where tarifa_express is null;
+update cliente set tarifa_express = tarifa_express + 3;
+update cliente set desaduanaje_express = desaduanaje_express + 3;
+alter table cliente
+    change tarifa_express tarifa_express decimal(5,2) not null default 28;
+alter table cliente
+    change desaduanaje_express desaduanaje_express decimal(5,2) not null default 28;
+alter table cliente
+    add seguro decimal(5, 2) default 0.02 not null;
