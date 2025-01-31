220b79be9c0cd64f0f052e67d7bb0ab423244451
jjse12 <jjse127@gmail.com>
2025-01-31 11:06:48 -0600

    Feature change

--- db/DBgetAndInsertNewUsers.php
+++ db/DBgetAndInsertNewUsers.php
@@ -5,8 +5,8 @@ require_once("server_db_vars.php");
 require_once("../classes/CosteadorPaquetes.php");
 
 try {
-	$localDB = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-	$serverDB = new mysqli(SERVER_DB_HOST, SERVER_DB_USER, SERVER_DB_PASS, SERVER_DB_NAME);
+    $localDB = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+    $serverDB = new mysqli(SERVER_DB_HOST, SERVER_DB_USER, SERVER_DB_PASS, SERVER_DB_NAME);
 
     $getLastClientSyncQuery = "SELECT fecha FROM sincronizacion_clientes ORDER BY fecha DESC LIMIT 1";
     $lastClientSyncResult = $localDB->query($getLastClientSyncQuery);
@@ -30,32 +30,27 @@ try {
     $tarifaExpress = CosteadorPaquetes::DEFAULT_TARIFA_EXPRESS;
     $desaduanaje = CosteadorPaquetes::DEFAULT_DESADUANAJE;
     $seguro = CosteadorPaquetes::DEFAULT_SEGURO;
-
-    if (!empty($coeficientesResult) && $coeficientesResult->num_rows > 0) {
-        $row = $coeficientesResult->fetch_assoc();
-
-        $tarifaExpress = (float)$row['tarifa'];
-        $desaduanaje = (float)$row['desaduanaje'];
-        $seguro = (float)$row['seguro'];
-    }
-
+    Change 5
+    Change 5
+    Change 5
+    Change 5
+    Change 5
     $values = "";
     while($client = mysqli_fetch_assoc($serverNewClientsResult)){
         $values .= "('{$client['chex_code']}', '{$client['first_name']}', '{$client['last_name']}', '{$client['email']}', '{$client['phone']}', '{$client['secondary_phone']}',
                  '{$client['department']}', '{$client['municipality']}', '{$client['zone']}', '{$client['address']}', '{$client['nit_name']}', '{$client['nit_number']}', '{$client['birthday']}',
                  '{$client['note']}', '{$client['meet_reason']}', '{$client['gender']}', '{$client['creation_date']}', $tarifaExpress, $desaduanaje, $seguro), ";
     }
+    Change 2
+    Change 2
     $values = substr($values, 0, -2);
     $serverNewClientsResult->close();
 
-    $insertNewClientsQuery = "INSERT INTO cliente(cid, nombre, apellido, email, celular, telefono_secundario,
-                    departamento, municipio, zona, direccion, nit_nombre, nit_numero, cumple, comentario,
-                    referencia, genero, fecha_registro, tarifa_express, desaduanaje_express, seguro)
-                VALUES $values";
+    Helloo tthis is a test
 
     $insertNewClientsResult = $localDB->query($insertNewClientsQuery);
     if (!$insertNewClientsResult) {
-        throw new RuntimeException($localDB->error);
+        throw new RuntimeException($Error);
     }
 
     $insertedClientsCount = $localDB->affected_rows;
@@ -63,6 +58,7 @@ try {
     echo json_encode(['numberOfClientsInserted' => $insertedClientsCount]);
 
     $localDB->query($insertNewClientSynchronizationQuery);
+    return 0;
 } catch (Exception $exception) {
     header("HTTP/1.1 500 Internal Server Error");
     echo json_encode(['errorMessage' => $exception->getMessage()]);
